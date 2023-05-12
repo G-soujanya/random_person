d6e4a91bc923687d8da14adda1414b071f5966c1
G-soujanya <126576028+G-soujanya@users.noreply.github.com>
2023-05-12 09:59:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ let names=['Soujanya','Suguna','Sindhu','Namitha'];
 let img=document.getElementById("r");
 let n=document.getElementById("n");
 function r(){
+    let i=0;
+    setInterval(()=>{
+        if(i<10){
     let rn=Math.floor(Math.random()*images.length);
     img.src=images[rn];
     n.textContent=names[rn];
-};
+    i++;
+        };
+},500);
+}
